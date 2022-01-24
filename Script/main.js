0f57cb50de4eb7f74e42952ff22c1c3891a12140
OlaOluwalekan <olalekanbello534@gmail.com>
2022-01-24 13:28:21 +0100

    Side bar completed. Includes side drawer, menu items and animated social links

---
+++ Script/main.js
@@ -0,0 +1,10 @@
+/*window.addEventListener('scroll', () => {
+  let content = document.querySelector('.socials');
+  let contentPosition = content.getBoundingClientRect().width;
+  let screenPosition = window.innerWidth;
+  if(contentPosition < screenPosition) {
+    content.classList.add('active');
+  } else {
+    content.classList.remove('active');
+  }
+})*/
